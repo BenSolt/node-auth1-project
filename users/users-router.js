bbc3d24dce9c4fa2fc50d76ef562821a1f6c5bcc
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-27 15:24:40 -0500

    added has info

--- users/users-router.js
+++ users/users-router.js
@@ -10,4 +10,70 @@ router.get('/', (req, res) => {
     .catch(err => res.send(err));
 });
 
+//GET USERS
+router.get('/users', (req, res,next) => {
+  //Users.find()
+  if(req.headers.authorization) {
+    bc.hash(req.headers.authorization), 8, (err, hash) => { 
+      // 10 is number of rounds
+      if (err) {
+        res.status(500).json({error: ' it borke'});
+      }else{
+      res.status(200).json({hash});
+      }
+    }
+  } else {
+    res.status(400).json({ error: 'missing header'});
+  }
+})
+
+//REGISTER USER (POST) create REGISTER
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+//////////////////////sets password to use hash
+    const hash = bc.hashSync(req.body.password, 8)
+
+    User.password = hash;
+/////////////////////////////////////////
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+
+// LOGIN USER (POST) - Create LOGIN
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bc.compareSync(password, user.password)) {
+           // if (user) {
+                // compare().then(match => {
+                //   if (match) {
+                //     // good password
+                //   } else {
+                //     // they don't match
+                //   }
+                // }).catch()
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+
+
+
 module.exports = router;
